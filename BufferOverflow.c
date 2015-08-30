bf6a67fd9ba8fcea6da5028c18aae15d3def627a
jackzhao-mj <jackzhao.mj@gmail.com>
2015-08-29 17:47:01 -0700

    nothing

--- BufferOverflow.c
+++ BufferOverflow.c
@@ -9,9 +9,19 @@ int main()
 {
 	char buf[2];
 	int authenticated = 0;
-	int a = 1, b = 2;
-	vulnerable(buf);
+	int a = 1, b = 2, a1 = 16;
+	float c = 3.00001, d = 4.001;
+	char c1, c2;
+
+	/*vulnerable(buf);*/
 	printf("%d\n", authenticated);
 	printf("%d %d \n", a, b);
+	printf("%3.1f %6f \n", c, d);
+	printf("%x\n", a1);
+	c1 = getchar();
+	c2 = '$';
+	putchar(c2);
+
+	printf("%c %c\n", c1, c2);
 	return 0;
 }
